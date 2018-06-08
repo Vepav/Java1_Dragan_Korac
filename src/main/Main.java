940ded589a87313be19f1d0f9e13fa9afbdfe8aa
Dragan <gagizmo79@gmail.com>
2018-06-08 23:48:28 +0200

    testing classes Radio and Television

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,3 @@
-
 package main;
 
 import media.Television;
@@ -6,14 +5,27 @@ import media.Radio;
 
 public class Main {
 
-    
     public static void main(String[] args) {
+
+        Television myTv = new Television();
+
+        myTv.info();
+
+        myTv.setcurrentProgram(5);
+        myTv.setTurnOn(true);
+        myTv.setVolume(50);
+
+        myTv.info();
+
+        myTv.setcurrentProgram(8);
+        myTv.setTurnOn(true);
+        myTv.setVolume(30);
+
+        myTv.info();
+
+      
+        Radio myRadio = new Radio(100.3, 700, 'A');
+        myRadio.info();
         
-        Television RTS = new Television();
-        RTS.showData();
-    
-        Radio TDI = new Radio();
-            TDI.showData();
-   
-}
+    }
 }
