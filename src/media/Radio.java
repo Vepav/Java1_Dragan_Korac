82cb0a43afe48ce4b25a70721a93719ccb9b17e9
Dragan <gagizmo79@gmail.com>
2018-06-08 23:45:35 +0200

    adds constructor for class Radio

--- src/media/Radio.java
+++ src/media/Radio.java
@@ -3,52 +3,53 @@ package media;
 public class Radio {
     
     private double fmFrequency;
-    private double amFrequency;
-    private boolean band;
-    
- public Radio() {
-     fmFrequency = 91.8;
-     amFrequency = 600;     
- }
-     
-     
- 
-    public Radio(double fmFrequency, double amFrequency, boolean band) {
+    private int amFrequency;
+    private char band;
+
+    public Radio() {
+        this.fmFrequency = 91.8;
+        this.amFrequency = 600;
+        this.band = 'F';
+    }
+
+    public Radio(double fmFrequency, int amFrequency, char band) {
         this.fmFrequency = fmFrequency;
         this.amFrequency = amFrequency;
         this.band = band;
-
     }
-   
-    public double getfmFrequency() {
+    
+    
+
+    public double getFmFrequency() {
         return fmFrequency;
     }
 
-    public void setfmFrequency(double fmFrequency) {
+    public void setFmFrequency(double fmFrequency) {
         this.fmFrequency = fmFrequency;
     }
 
-    public double getamFrequency() {
+    public int getAmFrequency() {
         return amFrequency;
     }
 
-    public void setamFrequency(double amFrequency) {
+    public void setAmFrequency(int amFrequency) {
         this.amFrequency = amFrequency;
     }
 
-    public boolean getband() {
+    public char getBand() {
         return band;
     }
 
-    public void setturnOn(boolean band) {
+    public void setBand(char band) {
         this.band = band;
-
+    }
+    public void info() {
+        System.out.println("FM Frequency is: " + getFmFrequency());
+        System.out.println("AM Frequency is: " + getAmFrequency());
+        System.out.println("Band is: " + getBand());
+      
     }
 
-    public void showData() {
-        System.out.println("FM frekvencija je: " + getfmFrequency());
-        System.out.println("AM frekvencija je: " + getamFrequency());
-        System.out.println("Band je : " + getband());
-    }
 
+    
 }
